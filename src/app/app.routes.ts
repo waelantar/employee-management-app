b9223dcd100dbc362a66adff76a6eaa35084c26e
Wael Antar <antarwael189@gmail.com>
2024-10-01 19:43:04 +0100

    5:feat configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: '/employees',
+      pathMatch: 'full',
+    },
+    {
+      path: 'employees',
+      loadChildren: () => import('./features/employee/employees.module').then(m=>m.EmployeesModule),
 
-export const routes: Routes = [];
+    },
+    {
+      path: '**',
+      component: PageNotFoundComponent,
+    },
+  ];
+
